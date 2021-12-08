67af975a14e57d2c7ccce8b2d53fb7a79f91608b
AnastasiaDro <adrogunova@gmail.com>
2021-12-09 00:20:30 +0300

    Initial commit

---
+++ src/main/kotlin/Contact.kt
@@ -0,0 +1,12 @@
+class Contact (private val fName : String,
+               private val secName : String,
+               private val nickName : String,
+               private val pNum : Int,
+               private val dSecret : String) {
+    fun getName() = fName
+    fun getSecName() = secName
+    fun getNickName() = nickName
+    fun getpNum() = pNum
+    fun getDSecret() = dSecret
+
+}
---
+++ src/main/kotlin/PhoneBook.kt
@@ -0,0 +1,18 @@
+class PhoneBook(private var contArr : ArrayList<Contact>, private var oldElem : Int = 1)  {
+    fun addContact(contact: Contact)
+    {
+        ////TODO
+//        if (contArr.size < 8)
+//        {
+//            contArr.add(contact)
+//        }
+//        else
+//        {
+//            oldElem = 0
+//            contArr.set(oldElem, contact)
+//
+//        }
+
+
+    }
+}
